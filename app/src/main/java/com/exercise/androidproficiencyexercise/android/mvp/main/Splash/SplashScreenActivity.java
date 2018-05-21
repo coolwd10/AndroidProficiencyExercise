20d8b7192e357e80adfbc6a792b6593c38e527cb
Akash Shah <akashshah@gmx.com>
2018-05-21 23:34:38 +0530

    Fixed Splash view issue.

--- app/src/main/java/com/exercise/androidproficiencyexercise/android/mvp/main/Splash/SplashScreenActivity.java
+++ app/src/main/java/com/exercise/androidproficiencyexercise/android/mvp/main/Splash/SplashScreenActivity.java
@@ -3,6 +3,10 @@ package com.exercise.androidproficiencyexercise.android.mvp.main.Splash;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.exercise.androidproficiencyexercise.ExerciseApp;
 import com.exercise.androidproficiencyexercise.R;
@@ -21,6 +25,19 @@ public class SplashScreenActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+        decorView.setSystemUiVisibility(uiOptions);
+
         initDI();
         new Handler().postDelayed(new Runnable() {
 
